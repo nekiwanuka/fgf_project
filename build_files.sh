9dbfa73def871f88a762944c7f4309423f4ae921
nekiwanuka <nekiwanuka@gmail.com>
2023-09-27 13:08:13 +0300

    hosted API

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt 
 python3.11 manage.py collectstatic
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
-python manage.py test
+# python manage.py makemigrations
+# python manage.py migrate
+# python manage.py collectstatic --noinput
+# python manage.py test
