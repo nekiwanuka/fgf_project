c3e82eab2cf527cec978c02b353a76d9918ee6b4
nekiwanuka <nekiwanuka@gmail.com>
2023-09-27 14:35:14 +0300

    modified:   plants_app/urls.py

--- animals_app/urls.py
+++ animals_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import AnimalListCreateView, AnimalDetailView, AnimalClassificationListCreateView, AnimalClassificationDetailView
+from animals_app.views import AnimalEntriesAPIView
+from .views import *
 
 urlpatterns = [
     path('animals/', AnimalListCreateView.as_view(), name='animal-list-create'),
@@ -7,4 +8,9 @@ urlpatterns = [
 
     path('animal-classifications/', AnimalClassificationListCreateView.as_view(), name='animal-classification-list-create'),
     path('animal-classifications/<int:pk>/', AnimalClassificationDetailView.as_view(), name='animal-classification-detail'),
+
+    # URLs for AnimalEntries Calculation view
+    path('animal_entries/', AnimalEntriesAPIView.as_view(), name='animal-entries'),
+
 ]
+
