f18da93673dea34e833a11ab83c6d5672169cb11
nekiwanuka <nekiwanuka@gmail.com>
2023-10-04 12:47:45 +0300

    Updated Requirements.txt

--- fgf/settings.py
+++ fgf/settings.py
@@ -96,11 +96,12 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+    
+"default": {
+    "ENGINE": "django.db.backends.sqlite3",
+    "NAME": BASE_DIR / "db.sqlite3",
+     }
+ }
 
 #DATABASES['default'] = dj_database_url.parse('postgres://fgf_database_user:Hqa4PKjnQGZa33ErpfAYCRpSujZmyp7M@dpg-cka1lbev3ddc739sufb0-a.oregon-postgres.render.com/fgf_database')
 # Password validation
