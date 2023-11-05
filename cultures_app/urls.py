26c16f2198a463c376bf6fedb54815febe6bfab9
paulie <pktpaulie@gmail.com>
2023-11-05 15:30:50 -0500

    Edit Culture Views to Remove Permission Classes Enable Post Method for Culture

--- cultures_app/urls.py
+++ cultures_app/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/cultures/', views.CulturalIdentityListview.as_view()),
+    path('api/cultures/', views.CulturalIdentityListCreateView.as_view(), name='animal-list-create'),
     path('cultures/<int:pk>/', views.CulturalIdentityDetailView.as_view()),
-    path('cultures/cultural_kingdom/', views.CulturalKingdomListview.as_view()),
+    path('cultures/cultural_kingdom/', views.CulturalKingdomListView.as_view()),
     path('cultures/cultural_kingdom/<int:pk>/', views.CulturalKingdomDetailView.as_view()),
-    path('cultures/ethnicities/', views.EthnicityListview.as_view()),
+    path('cultures/ethnicities/', views.EthnicityListView.as_view()),
     path('cultures/ethnicities/<int:pk>/', views.EthnicityDetailView.as_view()),
-    path('cultures/clans/', views.ClanListview.as_view()),
+    path('cultures/clans/', views.ClanListView.as_view()),
     path('cultures/clans/<int:pk>/', views.ClanDetailView.as_view()),
-    path('cultures/ethnic_groups/', views.EthnicGroupListview.as_view()),
+    path('cultures/ethnic_groups/', views.EthnicGroupListView.as_view()),
     path('cultures/ethnic_groups/<int:pk>/', views.EthnicGroupDetailView.as_view()),
      
 ]
